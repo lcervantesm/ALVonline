d6bcd0e62c2bd89ad742e4098a691ae64ec61964
Adal Loya <adalloya@gmail.com>
2019-01-30 14:13:28 -0600

    hello

--- client/src/components/Filter/index.js
+++ client/src/components/Filter/index.js
@@ -3,41 +3,29 @@ import "./style.css";
 
 function Filter() {
     return (
-    <div id="checkbox">
-    <h5 className="filter-div">Filtra por categoria</h5>
-        <form action="#">
-            <p>
-            <label>
-                <input htmlFor="videojuegos" type="checkbox" id="videojuegos" className="red"/>
-                <span className="letra">Videojuegos</span>
-            </label>
-            </p>
-            <p>
-            <label>
-                <input htmlFor="tecnologia" type="checkbox" id="tecnologia" className="checkbox"/>
-                <span className="letra">Tecnología</span>
-            </label>
-            </p>
-            <p>
-            <label>
-                <input htmlFor="hogar" type="checkbox" id="hogar" className="checkbox"/>
-                <span className="letra">Hogar</span>
-            </label>
-            </p>
-            <p>
-            <label>
-                <input htmlFor="servicios" type="checkbox" id="servicios" className="checkbox"/>
-                <span className="letra">Servicios</span>
-            </label>
-            </p>
-            <p>
-            <label>
-                <input htmlFor="otros" type="checkbox" id="otros" className="checkbox"/>
-                <span className="letra">Otros</span>
-            </label>
-            </p>
-        </form>
-    </div>
+        <div id="modal5" className="modal bottom-sheet">
+        <div className="modal-content">
+        <blockquote>
+    <h4>Filtrar por categoria</h4>
+  <a class="waves-effect waves-light btn filtros" id="Accesorios">Accesorios</a>
+  <a class="waves-effect waves-light btn filtros" id="Consolas">Consolas</a>
+  <a class="waves-effect waves-light btn filtros" id="Juegos">Juegos</a>
+  </blockquote>
+  <blockquote>
+    <h4>Filtrar por consola</h4>
+    <a class="waves-effect waves-light btn filtros" id="Accesorios">Nintendo 3DS</a>
+  <a class="waves-effect waves-light btn filtros" id="Clasico">Clasico</a>
+  <a class="waves-effect waves-light btn filtros" id="PS3">PS3</a>
+  <a class="waves-effect waves-light btn filtros" id="PS4">PS4</a>
+  <a class="waves-effect waves-light btn filtros" id="Switch">Switch</a>
+  <a class="waves-effect waves-light btn filtros" id="VITA">PSVita</a>
+  <a class="waves-effect waves-light btn filtros" id="WiiU">WiiU</a>
+  <a class="waves-effect waves-light btn filtros" id="Xbox">Xbox One</a>
+  </blockquote>
+ </div>
+       
+</div>
+    
     );
 }
 
--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -12,24 +12,40 @@ function Form() {
       <h4>Publica tu anuncío</h4>
     </blockquote>
              <form className="col s12">
-                             <div className="input-field col s12">
-                                <input id="item" type="text" data-length="30" maxLength="30"></input>
-                                <label htmlFor="item">¿Qué quieres vender?</label>
-                             </div>
-                            <div className="input-field col s12">
-                              <textarea id="description" className="materialize-textarea" data-length="240" maxLength="240"></textarea>
-                              <label htmlFor="description">¿Que nos cuentas de lo que vendes?</label>
-                            </div>
-                               <div className="input-field col s12">
+
+                        <div className="input-field col s12">
                                     <select id="category">
                                       <option value="" defaultValue>Selecciona una categoria</option>
-                                      <option value="Videojuegos">Videojuegos</option>
-                                      <option value="Tecnologia">Tecnologia</option>
-                                      <option value="Hogar">Hogar</option>
-                                      <option value="Servicios">Servicios</option>
-                                      <option value="Otros">Otros</option>
+                                      <option value="Accesorio">Accesorio</option>
+                                      <option value="Consola">Consola</option>
+                                      <option value="Juego">Juego</option>
+                                  
                                     </select>
-                                    <label>¿Qué tipo de venta es?</label>
+                                    <label>¿Qué es lo que vendes?</label>
+                             </div>
+
+                                <div className="input-field col s12">
+                                    <select id="consola">
+                                      <option value="" defaultValue>Selecciona la Consola</option>
+                                      <option value="Nintendo 3DS"> Nintendo 3DS</option>
+                                      <option value="Clasico">Clasico</option>
+                                      <option value="PS3">PS3</option>
+                                      <option value="PS4">PS4</option>
+                                      <option value="Switch">Switch</option>
+                                      <option value="Vita">Vita</option>
+                                      <option value="Vita">WiiU</option>
+                                      <option value="XBOX ONE">XBOX ONE</option>
+                                  
+                                    </select>
+                                    <label>¿Para que consola es?</label>
+                             </div>
+                             <div className="input-field col s12">
+                                    <select id="status">
+                                      <option value="" defaultValue>Selecciona la condicion</option>
+                                      <option value="Nuevo">Nuevo</option>
+                                      <option value="Usado">Usado</option>
+                                    </select>
+                                    <label>¿En que condicion está?</label>
                              </div>
                              <div className="input-field col s12">
                                     <select id="location">
@@ -43,16 +59,17 @@ function Form() {
                                     <label>¿Donde lo vendes?</label>
                              </div>
                              <div className="input-field col s12">
-                                    <select id="status">
-                                      <option value="" defaultValue>Selecciona la condicion</option>
-                                      <option value="Nuevo">Nuevo</option>
-                                      <option value="Usado">Usado</option>
-                                    </select>
-                                    <label>¿En que condicion está?</label>
+                                <input id="item" type="text" data-length="30" maxLength="30"></input>
+                                <label htmlFor="item">¿Como se llama tu producto?</label>
                              </div>
+                           
+                            <div className="input-field col s12">
+                              <textarea id="description" className="materialize-textarea" data-length="240" maxLength="240"></textarea>
+                              <label htmlFor="description">Describelo brevemente</label>
+                            </div>
                                         <div className="input-field col s12">
                                           <textarea id="price" type="number" className="materialize-textarea" data-length="10" maxLength="10"></textarea>
-                                          <label htmlFor="price">Precio</label>
+                                          <label htmlFor="price">¿En cuanto lo vendes?</label>
                                         </div>
                                       <div className= "col s12">
                                       <blockquote>
