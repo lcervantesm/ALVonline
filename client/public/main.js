cfbaa9d3ea5a4cae865d51e0bb42ac770cb840fd
lcervantesm <cervants2802@gmail.com>
2019-01-29 21:13:30 -0600

    pulling from master

--- client/public/main.js
+++ client/public/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    $("#uploadButton").hide();
+    $("#exito").hide();
+    $("#filerurl").hide();
   // Initialize Firebase-----------------------------------------------------------------
   var config = {
     apiKey: "AIzaSyC9sJ8GtsGTdC_wZ__CqZX29G03Gah-ns8",
@@ -13,19 +16,19 @@ $(document).ready(function() {
   var database = firebase.database();
 
   var toDoCount = 0;
-  var name= "";
-  var phone= "";
   var item= "";
   var description= "";
   var price= "";
   var category= "";
   var location= "";
+  var selectedFile= " ";
+
   var d = new Date();
 
   var month = d.getMonth()+1;
   var day = d.getDate();
   var time=d.getTime();
-  
+
   var output = (day<10 ? '0' : '') + day + '/' +
       (month<10 ? '0' : '') + month + '/' +
       d.getFullYear();
@@ -34,6 +37,7 @@ $(document).ready(function() {
 
 var connectionsRef = database.ref("/connections");
 var connectedRef = database.ref(".info/connected");
+var adnumber = database.ref("/ads");
 
 connectedRef.on("value", function(snap) {
   if (snap.val()) {
@@ -47,8 +51,14 @@ connectionsRef.on("value", function(snap) {
   $("#number1").append(""+ " "+"Personas comprando");
 });
 
+adnumber.on("value",function(snap) {
+$("#number").text(snap.numChildren());
+$("#number").append(""+ " "+"Articulos en venta");
+toDoCount= snap.numChildren();
+});
+
  // ON BUTTON SELECT FORM-----------------------------------------------------------------
- $('input#name, input#phone, input#item, input#price, input#description').characterCounter();
+ $('input#item, input#price, input#description').characterCounter();
  $('input#username, input#userphone, input#useremail, input#userlocation, input#userpassword').characterCounter();
 
  document.addEventListener('DOMContentLoaded', function() {
@@ -61,13 +71,12 @@ connectionsRef.on("value", function(snap) {
     var instances = M.Modal.init(elems, options);
   });
     $('.modal').modal();
+    $('.collapsible').collapsible();
     validate();
-    $('#name, #phone, #description, #category, #location, #price, #item').change(validate);
+    $('#description, #category, #location, #price, #item').change(validate);
 
 function validate(){
-    if ($('#name').val().length   >   0   &&
-        $('#phone').val().length  >   0   &&
-        $('#description').val().length  >   0   &&
+    if ($('#description').val().length  >   0   &&
         $('#category').val().length  >   0   &&
         $('#location').val().length  >   0   &&
         $('#price').val().length  >   0   &&
@@ -84,14 +93,18 @@ $('select').formSelect();
 $("#add-to-do").on("click", function(event) {
       event.preventDefault();
 
-      var name= $("#name").val().trim();
-      var phone= $("#phone").val().trim();
-      var email= $("#email").val().trim();
+      toDoCount++;
+
+      var name= userloginname;
+      var phone= userloginphone;
+      var email= userloginemail;
       var item= $("#item").val().trim();
       var description= $("#description").val().trim();
       var price= $("#price").val().trim();
       var category= $('select#category').val();
       var location= $('select#location').val();
+      var itemimage= null;
+      var imagecard= $("#fileurl").html();
 
       database.ref("ads").push({
         name: name,
@@ -104,6 +117,7 @@ $("#add-to-do").on("click", function(event) {
         location:location,
         dateposted:output,
         id:toDoCount,
+        image: imagecard,
         dateAdded: firebase.database.ServerValue.TIMESTAMP,
 
       });
@@ -118,10 +132,11 @@ $("#price").val("");
 $('#category').addClass();
 $("#location").addClass();
 $("#email").val("");
+$("#fileurl").html("");
 // Add to the toDoCount
-toDoCount++;
-$("#add-to-do").addClass("disabled")
-
+$("#add-to-do").addClass("disabled");
+$("#exito").hide();
+$("#uploadButton").hide();
       
     });
 
@@ -137,11 +152,10 @@ $("#new-register").on("click", function(event) {
     var userpassword= $('#userpassword').val();
     var useraviso= $('#aviso').val();
     var userid= 1;
-
-    console.log("hey");
+   
 
     database.ref("users").push({
-      username: username,
+     username: username,
       userphone: userphone,
       useremail: useremail,
       userlocation:userlocation,
@@ -155,6 +169,7 @@ $("#new-register").on("click", function(event) {
 $("#username").val("");
 $("#userphone").val("");
 $("#useremail").val("");
+$("#userpassword").val("");
 $("#phone").val("");
 $("#item").val("");
 $("#userlocation").val("");
@@ -164,33 +179,124 @@ $("#new-register").addClass("disabled")
 
     
   });
+  $("#loginbutton").on("click", function(event) {
+    event.preventDefault();
 
-$("#login").on("click", function(event) {
-  var users = firebase.database().ref("users");
-  console.log(users.path.pieces_[0]);
+
+    var emaillogin= $("#emaillogin").val().trim();
+    var contraseñalogin= $("#contraseñalogin").val().trim();
+
+
+    database.ref('users').orderByChild("useremail").equalTo(emaillogin).on("value", function(snapshot) {
+        snapshot.forEach(function(data) {
+          userlogin= data.val().userpassword;
+          userloginname= data.val().username;
+          userloginphone= data.val().userphone;
+          userloginlocation =data.val().userlocation;
+          userloginemail =data.val().useremail;
+
+        });
+
+        if (userlogin=contraseñalogin) {
+            
+            $("#loginmodal").html("Haz inciado sesion"+" "+ userloginname + "<hr>"+"Puedes publicar con el icono rojo y el signo de mas");
+            $('#posticon').removeClass("hide");
+            $("#nombre").html(userloginname);
+            $("#phone").html(userloginphone);
+            $("#email").html(userloginemail);
+            $('#usericon').addClass("hide");
+            $('#viewicon').removeClass("hide");
+
+            database.ref('ads').orderByChild("email").equalTo(userloginemail).on("value", function(snapshot) {
+                snapshot.forEach(function(data) {
+                    $("#userads").html(
+                "<li>"+
+                  "<div className='collapsible-header'>"+"<i className='material-icons'>filter_drama</i>"+snapshot.val().item+"</div>"+
+                  "<div className='collapsible-body'><span>"+snapshot.val().description+"</span></div>"+
+               "</li>"
+              );  
+                });
+            
+            });
+
+          } else {
+            console.log("The email or password is incorrect");
+            $("#status").html("The email or password is incorrect, please try again");
+          }
+    });
 
   });
+  
+  $("#file").on("change",function(event){
+    selectedFile= event.target.files[0];
+    $("#uploadButton").show();
+
+  });
+
+  $("#uploadButton").on("click", function uploadFile() {
+    var filename = selectedFile.name;
+    var storageRef = firebase.storage().ref('/itemimages/'+ filename);
+     var uploadTask= storageRef.put(selectedFile);
+
+     uploadTask.on('state_changed', function(snapshot){
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      }, function(error) {
+        // Handle unsuccessful uploads
+      }, function() {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+          console.log('File available at', downloadURL);
+          itemimage= downloadURL;
+          $("#fileurl").html(itemimage);
+          $("#fileurl").addClass("hide");
+          $("#exito").show();
+        
+        });
+      });
+        });
 //DELETE------------------------------------------------------------------------------------------------------------------------ 
     // When a user clicks a check box then delete the specific content
     // (NOTE: Pay attention to the unusual syntax here for the click event.
     // Because we are creating click events on "dynamic" content, we can't just use the usual "on" "click" syntax.)
-    $(document.body).on("click", ".checkbox", function() {
+    $(document.body).on("click", ".checkbox", function() { 
       var toDoNumber = $(this).attr("data-to-do");
       $("#item-" + toDoNumber).remove();
     });
    // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
    database.ref("ads").on("child_added", function(childSnapshot) {
+    // full list of items to the well
+    // Handle the errors
+  }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
+
+  database.ref("ads").on("child_added", function(childSnapshot) {
+
     $("#number").html(childSnapshot.val().id+ " "+ "Articulos en venta");
     // full list of items to the well
-    $("#to-dos").append(
-        "<div id="+childSnapshot.val().id+" " + "class='card col s12 m4 l4 xl3 hoverable card small'>"+
+    $("#to-dos").prepend(
+        "<div id="+childSnapshot.val().id+" " + "class='card col s12 m4 l4 xl3 hoverable card'>"+
         "<div class='card-image waves-effect waves-block waves-light'>" +
-        "<img class='activator center-align responsive-img' src="+"'"+childSnapshot.val().category+"'"+"></img>"+
+        "<img class='activator cardimage' src="+"'"+childSnapshot.val().image+"'"+"></img>"+
         "</div>"+
         "<div class='card-content'>"+
-        "<span class='card-title activator grey-text text-darken-4'flow-text>"+childSnapshot.val().item+"</span><i class='material-icons right'>more_vert</i>"+
+        "<span class='activator titulo'>"+childSnapshot.val().item+"</span><i class='material-icons right'>more_vert</i>"+
         "<p><a href="+"'"+childSnapshot.val().phone+"'"+ "class='waves-effect waves-light btn-small contactar flow-text'>Contactar</a></p>"+
         "</div>"+
+
+
         "<div class='card-reveal'>"+
         "<span class='card-title grey-text text-darken-4 flow-text'>"+childSnapshot.val().item+"<i class='material-icons right'>close</i></span>"+
         "<p class='flow-text'>"+childSnapshot.val().description+"</p>"+
@@ -198,18 +304,17 @@ $("#login").on("click", function(event) {
         "</div>"+
         "</div>"
     
-);
+);  
+$("#userads").prepend(
+"<li class= 'collection-item avatar'>"+
+    "<div class='collapsible-header'>"+"<i class='material-icons'>"+"<img class='circle' src="+"'"+childSnapshot.val().image+"'"+"></img>"+"</i>"+childSnapshot.val().item+"</div>"+
+    "<div class='collapsible-body'>"+"<span>"+childSnapshot.val().description+"</span>"+"</div>"+
+ "</li>");  
 
-  
     // Handle the errors
   }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
   });
 
- ///console log users
- database.ref("users").on("value", function(childSnapshot) {
-  console.log(childSnapshot.val())
-}, function(errorObject) {
-console.log("Errors handled: " + errorObject.code);
-});
+
 });
